555aebf5b25bcfd75c077cdcf62b9fc47021acf4
Fidele <crescens.kob@outlook.fr>
2017-10-29 20:43:18 +0800

    Authorizing deletions for user or admin

--- src/main/java/com/smartsoft/review/ReviewRepository.java
+++ src/main/java/com/smartsoft/review/ReviewRepository.java
@@ -2,6 +2,16 @@ package com.smartsoft.review;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 public interface  ReviewRepository extends PagingAndSortingRepository<Review, Long> { // this will create CRUD automatically
+
+    @Override
+    @PreAuthorize("hasRole('ROLE_ADMIN') or @reviewRepository.findOne(#id)?.reviewer?.username == authentication.name")
+    void delete(@Param("id") Long id);
+
+    @Override
+    @PreAuthorize("hasRole('ROLE_ADMIN') or #review.reviewer?.username == authentication.name")
+    void delete(@Param("review") Review entity);
 }
